d4cdcbfd3ebffc8d0cd75215d468cadffb37059e
rubenjz <jimenezruben304@com>
2023-04-12 17:01:12 -0500

    pato

---
+++ Demo2/src/demo2/Demo2.java
@@ -0,0 +1,9 @@
+package demo2;
+
+public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.printf("%s%n","Loja, una ciudad llena de arte y naturaleza con 400322");
+    }
+    
+}
